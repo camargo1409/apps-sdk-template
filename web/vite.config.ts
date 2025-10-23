2821a64b69140e49db368e165cd3a32c88de726a
Julien Vallini <vallinij@gmail.com>
2025-10-23 15:39:04 +0200

    Move vite plugin to skybridge

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -2,7 +2,7 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
-import { skybridge } from "./plugin";
+import { skybridge } from "skybridge/web";
 
 // https://vite.dev/config/
 export default defineConfig({
