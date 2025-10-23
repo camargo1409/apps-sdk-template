684e7e605cf946812b1eddea22aaa3a2edbe151f
Julien Vallini <vallinij@gmail.com>
2025-10-23 14:01:45 +0200

    Export specific vite config in a plugin

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,28 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import { globSync } from "glob";
-import path, { resolve } from "path";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
+import { skybridge } from "./plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  build: {
-    cssCodeSplit: false,
-    rollupOptions: {
-      // Detects all files in widgets folder as entry points
-      input: Object.fromEntries(
-        globSync("src/widgets/*.{js,ts,jsx,tsx,html}").map((file) => {
-          return [file.match(/^src\/widgets\/(.+)\.tsx$/)?.[1] ?? file.slice(10, -3), resolve(__dirname, file)];
-        }),
-      ),
-      output: {
-        entryFileNames: "[name].js", // Necessary to generate consistently named widget entry points
-        assetFileNames: "[name][extname]", // Necessary alongside cssCodeSplit: false to generate a unique style.css file
-      },
-    },
-    minify: true,
-  },
+  plugins: [skybridge(), react(), tailwindcss()],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
